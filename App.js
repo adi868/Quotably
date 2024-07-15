c4de0f75fcfe72673a01a0f14c04c96e018bb1e2
Adina <alcianciotto@gmail.com>
2024-07-14 18:14:28 -0700

    Fading animations

--- App.js
+++ App.js
@@ -1,24 +1,82 @@
 import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createStackNavigator } from '@react-navigation/stack';
+import { StyleSheet} from 'react-native';
 import IntroScreen from './screens/IntroScreen';
 import HomeScreen from './screens/HomeScreen';
 import AddQuote from './screens/AddQuoteScreen';
 import SplashScreen from './screens/SplashScreen';
-import { StyleSheet, Text, View } from 'react-native';
 
-const Stack = createNativeStackNavigator();
+const Stack = createStackNavigator();
+
+const fadeAnimation = ({ current }) => ({
+  cardStyle: {
+    opacity: current.progress,
+  },
+});
 
 export default function App() {
   return (
-      <NavigationContainer style={styles.container}>
-        <Stack.Navigator initialRouteName='Splash'>
-          <Stack.Screen name='Splash' component={SplashScreen} options={{ headerShown: false }} />
-          <Stack.Screen name='Intro' component={IntroScreen} options={{ headerShown: false }} />
-          <Stack.Screen name='Home' component={HomeScreen} options={{ headerShown: false }} />
-          <Stack.Screen name='AddQuote' component={AddQuote} options={{ headerShown: false }} />
-        </Stack.Navigator>
-      </NavigationContainer>
+    <NavigationContainer style={styles.container}>
+      <Stack.Navigator initialRouteName='Splash'>
+        <Stack.Screen name='Splash' component={SplashScreen} options={{ headerShown: false }} />
+        <Stack.Screen name='Intro' component={IntroScreen} options={{
+            headerShown: false,
+            cardStyleInterpolator: fadeAnimation,
+            transitionSpec: {
+              open: {
+                animation: 'timing',
+                config: {
+                  duration: 800, 
+                },
+              },
+              close: {
+                animation: 'timing',
+                config: {
+                  duration: 800,
+                },
+              },
+            },
+        }} />
+        <Stack.Screen name='Home' component={HomeScreen} options={{
+            headerShown: false,
+            cardStyleInterpolator: fadeAnimation,
+            transitionSpec: {
+              open: {
+                animation: 'timing',
+                config: {
+                  duration: 300, 
+                },
+              },
+              close: {
+                animation: 'timing',
+                config: {
+                  duration: 300,
+                },
+              },
+            },
+        }}
+        />
+        <Stack.Screen name='AddQuote' component={AddQuote} options={{
+            headerShown: false,
+            cardStyleInterpolator: fadeAnimation,
+            transitionSpec: {
+              open: {
+                animation: 'timing',
+                config: {
+                  duration: 400, 
+                },
+              },
+              close: {
+                animation: 'timing',
+                config: {
+                  duration: 400,
+                },
+              },
+            },
+        }} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
